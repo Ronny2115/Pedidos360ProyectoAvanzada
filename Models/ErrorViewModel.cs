66061f66cad14eb58968c47bd8a4af3d7124aa8c
Ronny2115 <andres05bonilla@gmail.com>
2026-07-05 22:47:26 -0600

    creacion

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace Pedidos360Proyecto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
+dwdwqdqwdqw
